a1bc4b956a013c2aab1f55377e5375726567c401
Hyejin Kim <khyejin08@naver.com>
2025-03-19 16:05:40 +0900

    header include 기능 구현

--- js/main.js
+++ js/main.js
@@ -1,58 +1,60 @@
-// header btn-search show/hide
-$(".btn-search").on("click", function () {
-  if (window.matchMedia("(min-width: 1024px)").matches) {
-    console.log("search-bar is hidden?", $(".search-bar").is(":hidden"));
-    if ($(".search-bar").is(":hidden")) {
-      $(".search-bar").show();
-    } else {
-      if ($(".search-bar").val() !== "") {
-        // 검색어가 입력되어 있을 경우 동작할 코드
-        return;
+$("header").load("/include/header.html", function () {
+  // header btn-search show/hide
+  $(".btn-search").on("click", function () {
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+      console.log("search-bar is hidden?", $(".search-bar").is(":hidden"));
+      if ($(".search-bar").is(":hidden")) {
+        $(".search-bar").show();
       } else {
-        $(".search-bar").hide();
+        if ($(".search-bar").val() !== "") {
+          // 검색어가 입력되어 있을 경우 동작할 코드
+          return;
+        } else {
+          $(".search-bar").hide();
+        }
       }
     }
-  }
-});
+  });
 
-// mobile-nav open
-$(".mobile-nav-open").on("click", function () {
-  $(".lnb").css("display", "none");
-  $(".nav").addClass("active");
-});
+  // mobile-nav open
+  $(".mobile-nav-open").on("click", function () {
+    $(".lnb").css("display", "none");
+    $(".nav").addClass("active");
+  });
 
-// mobile-nav close
-$(".mobile-nav-close").on("click", function () {
-  $(".nav").removeClass("active");
-});
-
-// mobile-nav close (사이드바 외부 클릭 시 닫힘)
-$(document).on("mouseup", function (event) {
-  if ($(".nav").has(event.target).length === 0) {
+  // mobile-nav close
+  $(".mobile-nav-close").on("click", function () {
     $(".nav").removeClass("active");
-  }
-});
+  });
 
-// mobile-search open
-$(".mobile-search-open").on("click", function () {
-  $(".mobile-search").addClass("active");
-});
+  // mobile-nav close (사이드바 외부 클릭 시 닫힘)
+  $(document).on("mouseup", function (event) {
+    if ($(".nav").has(event.target).length === 0) {
+      $(".nav").removeClass("active");
+    }
+  });
 
-// mobile-search open
-$(".mobile-search-close").on("click", function () {
-  $(".mobile-search").removeClass("active");
-});
+  // mobile-search open
+  $(".mobile-search-open").on("click", function () {
+    $(".mobile-search").addClass("active");
+  });
 
-// mobile-nav item open
-$(".menu-item-wrap").on("click", function () {
-  let $lnb = $(this).next();
-  let $chevron = $(this).children("i");
+  // mobile-search open
+  $(".mobile-search-close").on("click", function () {
+    $(".mobile-search").removeClass("active");
+  });
 
-  if ($lnb.css("display") == "none") {
-    $chevron.css("transform", "translateY(-50%) rotate(180deg)");
-    $lnb.slideDown(100);
-  } else {
-    $chevron.css("transform", "translateY(-50%) rotate(0deg)");
-    $lnb.slideUp(100);
-  }
+  // mobile-nav item open
+  $(".menu-item-wrap").on("click", function () {
+    let $lnb = $(this).next();
+    let $chevron = $(this).children("i");
+
+    if ($lnb.css("display") == "none") {
+      $chevron.css("transform", "translateY(-50%) rotate(180deg)");
+      $lnb.slideDown(100);
+    } else {
+      $chevron.css("transform", "translateY(-50%) rotate(0deg)");
+      $lnb.slideUp(100);
+    }
+  });
 });
