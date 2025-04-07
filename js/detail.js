6a44e95b135b866292497508abecbf7d81bc5ab2
Hyejin Kim <khyejin08@naver.com>
2025-04-07 16:45:14 +0900

    detail page jump-to-recipe 버튼 기능 구현

--- js/detail.js
+++ js/detail.js
@@ -3,3 +3,15 @@ $(".review-item").each(function () {
     $(this).children(".review").css("margin-bottom", "0");
   }
 });
+
+// jump-to-recipe
+$(".jump-to-recipe").on("click", function () {
+  // $("#recipe-start")[0].scrollIntoView({ behavior: "smooth" });
+  let scrollValue = $("#recipe-start").offset().top - 100;
+  $("html, body").animate(
+    {
+      scrollTop: scrollValue,
+    },
+    1000
+  );
+});
